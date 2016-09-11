87da756205bae065931c8d7cde93048fc081c3bc
= <=>
2016-09-10 18:55:37 -0700

    Style Change

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Declare our port, the database we will use, and our secret used for hashing.
 module.exports = {
-	'port': process.env.PORT || 3000,
+	'port': process.env.PORT || 3001,
 	'database': 'mongodb://localhost/bartenderApp',
 	'secret': 'FF0E116311AS2AC5DJNKQ!OUSAOIH@!EKJ SDHBJ	OIH@*H@DOKDL<'
 };

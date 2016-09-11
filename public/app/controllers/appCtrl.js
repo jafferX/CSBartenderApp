d6860179ade702f637dce93bc7e9596d73afa03a
= <=>
2016-09-10 19:15:31 -0700

    Added Examples

--- public/app/controllers/appCtrl.js
+++ public/app/controllers/appCtrl.js
@@ -1,6 +1,24 @@
 // We will put app logic here.
 angular.module('appCtrl', [])
 	.controller('appController', function() {
+		var vm = this;
 
-		
+		var drink1 = {
+			'pic': 'http://www.stonebrewing.com/sites/default/files/beer/menu/ipa_22_bottle.png',
+			'name': 'Stone IPA'
+		};
+
+		var drink2 = {
+			'pic': 'http://mixnsip.com/wp-content/uploads/2011/02/drink-0586ebe70e4c.jpg',
+			'name': 'Appletini'
+		};
+
+		var drink3 = {
+			'pic': 'http://images.wisegeek.com/cranberry-juice.jpg',
+			'name': 'Cranberry Juice'
+		};
+
+
+		vm.drinks = [drink1, drink2, drink3];
+
 	});

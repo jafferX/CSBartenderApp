aba385ec65432521acc787a940cb45971be1ac81
jaffer <eriklab1222@yahoo.com>
2016-09-21 13:16:40 -0700

    added api getbyDrink service

--- public/app/services/appService.js
+++ public/app/services/appService.js
@@ -7,5 +7,12 @@ angular.module('appService', [])
 			return $http.get('/api/ingredient/' + name);
 		};
 
+		appFactory.getByDrink = function(ing_name) {
+			 var xhr = new XMLHttpRequest();
+			 xhr.open("GET", "http://addb.absolutdrinks.com/drinks/with/" + ing_name + "?apiKey=3333501cb1af4603beccb822dc764f03", false);
+			 xhr.send();
+			 var responseString.JSOM.parse(xhr.responseText);
+		};
+
 		return appFactory;
 	});
